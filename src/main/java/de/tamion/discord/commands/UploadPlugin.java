6004444a62722d361d0353e979cd1a5edeb4b5de
Tamion <simon.dorner.obf@gmail.com>
2023-04-30 13:55:59 +0200

    Discord: Some Changes + Added Download From URl Command

---
+++ src/main/java/de/tamion/discord/commands/UploadPlugin.java
@@ -0,0 +1,34 @@
+package de.tamion.discord.commands;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.jetbrains.annotations.NotNull;
+
+public class UploadPlugin extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent e) {
+        if(e.getName().equals("uploadplugin")) {
+            e.deferReply().queue();
+            if(!e.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+                if(!e.getMember().getRoles().containsAll(e.getGuild().getRolesByName("PluginPerms", true)) || e.getGuild().getRolesByName("PluginPerms", true).isEmpty()) {
+                    e.getHook().sendMessage("You aren't allowed to upload Plugins!").setEphemeral(true).queue();
+                    return;
+                }
+            }
+            OptionMapping file = e.getOption("file");
+            OptionMapping name = e.getOption("name");
+            if(file == null) {
+                e.getHook().sendMessage("Please provide a valid file").queue();
+                return;
+            }
+            if(name == null) {
+                e.getHook().sendMessage("Please provide a valid filename").queue();
+                return;
+            }
+            file.getAsAttachment().downloadToFile("./plugins/" + name.getAsString());
+            e.getHook().sendMessage("Successfully uploaded Plugin").setEphemeral(true).queue();
+        }
+    }
+}
