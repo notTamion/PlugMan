a213802685c60523d549e5acc5a52e16bafbf517
Tamion <simon.dorner.obf@gmail.com>
2023-04-30 08:55:20 +0200

    Initial Commit

---
+++ src/main/java/de/tamion/commands/ClearConfig.java
@@ -0,0 +1,13 @@
+package de.tamion.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ClearConfig implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
