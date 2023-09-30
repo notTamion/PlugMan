aa7189d4956cba18df9e8abf968fd45172598bb1
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:56:43 +0200

    Added DisableAll and EnableAll Commands

--- src/main/java/de/tamion/commands/EnableAll.java
+++ src/main/java/de/tamion/commands/EnableAll.java
@@ -1,2 +1,26 @@
-package de.tamion.commands;public class EnableAll {
+package de.tamion.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.jetbrains.annotations.NotNull;
+
+public class EnableAll implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if(sender.hasPermission("PlugMan.enableall")) {
+            PluginManager pluginManager = Bukkit.getPluginManager();
+            for(Plugin pl : pluginManager.getPlugins()) {
+                pluginManager.enablePlugin(pl);
+                sender.sendMessage(pl.getName() + " has been enabled");
+            }
+            sender.sendMessage("All Plugins successfully enabled");
+        } else {
+            sender.sendMessage("You aren't allowed to execute this command!");
+        }
+        return false;
+    }
 }
