fc86533310845d6ec915b5747359b2c29611b278
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:56:43 +0200

    Initial Commit

---
+++ src/main/java/de/tamion/PlugMan.java
@@ -0,0 +1,27 @@
+package de.tamion;
+
+import de.tamion.commands.TogglePlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlugMan extends JavaPlugin {
+
+    private static PlugMan plugin;
+    @Override
+    public void onEnable() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        plugin = this;
+
+        getCommand("toggleplugin").setExecutor(new TogglePlugin());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static PlugMan getPlugin() {
+        return plugin;
+    }
+}
---
+++ src/main/java/de/tamion/commands/TogglePlugin.java
@@ -0,0 +1,39 @@
+package de.tamion.commands;
+
+import de.tamion.PlugMan;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.jetbrains.annotations.NotNull;
+
+public class TogglePlugin implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if(sender.hasPermission("PlugMan.toggle")) {
+            if(args.length == 1) {
+                String Pluginname = args[0];
+                PluginManager pluginManager = Bukkit.getPluginManager();
+                Plugin plugin = pluginManager.getPlugin(Pluginname);
+                if(plugin != null && plugin.getName() != "PlugMan") {
+                    if(plugin.isEnabled()) {
+                        pluginManager.disablePlugin(plugin);
+                        sender.sendMessage(plugin.getName() + " has been successfully disabled");
+                    } else {
+                        pluginManager.enablePlugin(plugin);
+                        sender.sendMessage(plugin.getName() + " has been successfully enabled");
+                    }
+                } else {
+                    sender.sendMessage("Plugin doesn't exist");
+                }
+            } else {
+                sender.sendMessage("Invalid Args!");
+            }
+        } else {
+            sender.sendMessage("You aren't allowed to execute this command!");
+        }
+        return false;
+    }
+}
