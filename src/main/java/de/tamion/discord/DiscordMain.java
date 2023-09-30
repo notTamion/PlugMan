19330b6d5b28d190acb3e038a15454d5fa5bc83d
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:56:44 +0200

    Discord: Added Discord Main and DownloadPlugin command

---
+++ src/main/java/de/tamion/discord/DiscordMain.java
@@ -0,0 +1,50 @@
+package de.tamion.discord;
+
+import de.tamion.discord.commands.DownloadPlugin;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class DiscordMain {
+    public static JDA jda;
+    public static <BufferedReader> void startup() {
+        try {
+            File tokenfl = new File("./plugins/PlugMan/token.txt");
+            if(!tokenfl.exists()) {
+                System.out.println("No Bot token set! To use Discord Please create the File Server/plugins/PlugMan/token.txt and paste your bot token in it.");
+                return;
+            }
+            String token = FileUtils.readFileToString(tokenfl);
+            jda = JDABuilder.createDefault(token)
+                    .addEventListeners(new DownloadPlugin())
+                    .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                    .setStatus(OnlineStatus.ONLINE)
+                    .setActivity(Activity.watching("Minecraft Server"))
+                    .build()
+                    .awaitReady();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Bot Started");
+        jda.upsertCommand("downloadplugin", "Downloads plugin onto server")
+                .addOption(OptionType.ATTACHMENT, "file", "File to download", true)
+                .addOption(OptionType.STRING, "name", "Name the file shall have", true)
+                .queue();
+    }
+    public static void shutdown() {
+        if(jda != null) {
+            jda.shutdown();
+        } else {
+            System.out.println("CANT STOP BOT! BOT NOT RUNNING!");
+        }
+    }
+    public static void restart() {
+        shutdown();
+        startup();
+    }
+}
